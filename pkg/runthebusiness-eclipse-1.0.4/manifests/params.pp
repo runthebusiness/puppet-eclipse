cae8ce7ac06b2bc5fc423fc091c69c14f7d38449
Will Ferrer <will.ferrer@runthebusiness.net>
2013-01-15 17:54:04 -0800

    - added the ability for plugins to be able to check if they are already installed before attemping to install them - added detection for the plugins that are defaultly supported by the module so they will not be reinstalled when puppet is run - removed the known issues section from the readme and module files as these new features resolve this issue - added version 1.0.4 to github and puppet forge

---
+++ pkg/runthebusiness-eclipse-1.0.4/manifests/params.pp
@@ -0,0 +1,43 @@
+# Class: eclipse::params
+#
+# Params for eclipse based on flavor of system
+#
+# Parameters:
+#  - downloadpath: where the wget file downloads to
+#  - installpath: where the installed package should go
+#  - desktopfilepath: if set this is the desktop file which will be modified wo include a link to eclipse
+#  - simlinkto: where to copy a simlink to
+
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class eclipse::params {
+
+	case $::operatingsystem {
+		'centos', 'redhat', 'fedora': {
+				#TODO: used the debian defaults -- verify in other flavors:
+				$downloadpath='/usr/src/'
+				$installpath='/usr/lib/'
+				$desktopfilepath='/usr/share/applications/'
+				$simlinkto='/usr/bin/'
+	    }
+	    'ubuntu', 'debian': {
+				$downloadpath='/usr/src/'
+				$installpath='/usr/lib/'
+				$desktopfilepath='/usr/share/applications/'
+				$simlinkto='/usr/bin/'
+	    }
+	    default: {
+		    #TODO: used the debian defaults -- verify in other flavors:
+				$downloadpath='/usr/src/'
+				$installpath='/usr/lib/'
+				$desktopfilepath='/usr/share/applications/'
+				$simlinkto='/usr/bin/'
+		}
+	}
+	$execlaunchpaths = ["/usr/bin", "/usr/sbin", "/bin", "/sbin", "/etc"]
+	$executefrom = "/tmp/"
+}
