b348d8c01392b6c07117f3f0877b0cc9e0495abe
mtbvang <mtb.vang@gmail.com>
2014-11-05 20:26:25 +0100

    Adding missing dollar sign to variable name.

--- manifests/init.pp
+++ manifests/init.pp
@@ -135,7 +135,7 @@ define eclipse(
 	    }
   	}
 	
-	if pluginius != undef {
+	if $pluginius != undef {
 		::eclipse::plugin{"eclipseinstallplugins":
 			timeout=>$timeout,
 			pluginrepositories=>$pluginrepositories,
